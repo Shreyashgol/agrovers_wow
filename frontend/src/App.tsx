4b80f100c56bd7be502c1ac4cbf4653a5dfc9cc5
Shreyash Golhani <shreyashgolhani@gmail.com>
2026-04-08 01:30:22 +0530

    some changes made

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,6 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Language } from './api/client';
-import { ThemeProvider } from './contexts/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
 import LandingPage from './pages/LandingPage';
 import LanguageSelector from './components/LanguageSelector';
 import NewSoilWizard from './pages/NewSoilWizard';
@@ -23,9 +21,7 @@ function App() {
   };
 
   return (
-    <ThemeProvider>
       <BrowserRouter>
-        <ThemeToggle />
         <Routes>
           {/* Landing Page - Default Route */}
           <Route path="/" element={<LandingPage />} />
@@ -46,7 +42,6 @@ function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
-    </ThemeProvider>
   );
 }
 
